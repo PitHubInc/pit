e1bc55582fe90cb88e608832b7bd65c9b731b4cf
EricJPogue <epogue@epogue.com>
2023-05-17 09:38:16 -0500

    Update files.

--- scripts/build.py
+++ scripts/build.py
@@ -1,7 +1,8 @@
 import os
 import subprocess
 
-subprocess.run(["python3", "scripts/update-build-number.py"])
+# Removing automatic update of build number. We may want to put this back in later.
+# subprocess.run(["python3", "scripts/update-build-number.py"])
 
 print("go build -o pit")
 os.system("go build -o pit")
